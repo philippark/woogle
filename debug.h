6e09f55c7d955e2f027727d4a7b4a8c4c6dd534a
philippark <philippark271@gmail.com>
2025-09-07 12:13:33 -0400

    add initial disassembler for chunks

---
+++ debug.h
@@ -0,0 +1,9 @@
+#ifndef woogle_debug_h
+#define woogle_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
