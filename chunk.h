99b14118d374cb35fdabf56371906a54a966cfce
philippark <philippark271@gmail.com>
2025-09-08 23:12:06 -0400

    add and integrate data structure to hold large constants

--- chunk.h
+++ chunk.h
@@ -2,6 +2,7 @@
 #define woogle_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
     OP_RETURN,
@@ -11,10 +12,12 @@ typedef struct {
     int count; // number of elements currently in the array
     int capacity; // total capacity of the array
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
